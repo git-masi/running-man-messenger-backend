a1d6e955c882cf77d5f4c6e088829e9c05701403
Eric Masi <eric@gitmasi.com>
2020-05-02 10:32:57 -0400

    Handle case, client emits join event

--- index.js
+++ index.js
@@ -1,24 +1,28 @@
 // Core Node Modules
-const http = require('http');
+const http = require("http");
 
 // Modules
-const chalk = require('chalk');
-const express = require('express');
-const socketio = require('socket.io');
+const chalk = require("chalk");
+const express = require("express");
+const socketio = require("socket.io");
 
 // Required Files
-const router = require('./routes/router');
+const router = require("./routes/router");
 
 const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
 
-io.on('connection', socket => {
-  console.log('New socket connected');
+io.on("connection", (socket) => {
+  console.log("New socket connected");
 
-  socket.on('disconnect', () => {
-    console.log('Someone has disconnected');
-  })
+  socket.on("join", ({ nickname, room }) => {
+    console.log(nickname, room);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("Someone has disconnected");
+  });
 });
 
 app.use(router);
@@ -28,4 +32,4 @@ const port = process.env.PORT || 3333;
 server.listen(port, (err) => {
   if (err) return console.log(err);
   console.log(`Server running ${chalk.blue.bold(`http://127.0.0.1:${port}`)}`);
-})
+});
