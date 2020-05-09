b971f320867acf2da43e9af8dfa5742807bf3ccb
Eric Masi <eric@gitmasi.com>
2020-05-09 11:53:09 -0400

    Add user model

---
+++ src/models/User.js
@@ -0,0 +1,40 @@
+const mongoose = require("mongoose");
+const validator = require("validator");
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+    lowercase: true,
+    validate(value) {
+      if (!validator.isEmail(value)) {
+        throw new Error("Email is invalid");
+      }
+    },
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 8,
+    validate(value) {
+      const passwordStrengthRegex = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*+]).*/;
+      if (!passwordStrengthRegex.text(value)) {
+        throw new Error(
+          "Password must contain at least 1 upper case letter, 1 lower case letter, 1 number, and 1 special character"
+        );
+      }
+    },
+  },
+});
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
